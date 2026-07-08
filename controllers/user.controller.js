858d990bedd51afab3403c055ce67d48cd665312
nikhilsaisrikarn@gmail.com <nikhilsaisrikarn@gmail.com>
2026-07-08 11:30:50 +0530

    add: get user data from id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,7 +4,17 @@ export async function getAllUsers(req, res, next) {
 
 export async function getUserData(req, res, next) {
     try {
+        const user = await User.findById(req.params.id);
 
+        if(!user) {
+            throw new ApiError(404, "User not found");
+        }
+
+        res.status(201).send({
+            success: true,
+            message: "User Found",
+            data: user
+        });
     } catch (e) {
         next(e);
     }
